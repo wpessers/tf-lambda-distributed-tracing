ecba9ed8cd732f9ebf762aaa8a9832cd3df6f05b
warre <warre.pessers@gmail.com>
2025-01-21 07:19:58 +0100

    Add dummy lambda handlers

---
+++ src/monitorLaunch.ts
@@ -0,0 +1,9 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const handler = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+    return {
+        statusCode: 200,
+        body: ""
+    }
+}
