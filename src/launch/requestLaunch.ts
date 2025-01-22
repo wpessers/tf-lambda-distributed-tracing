68a9439e6c8fdfb8e0a2989c384b2a52b330a84f
warre <warre.pessers@gmail.com>
2025-01-22 19:45:34 +0100

    Add some basic launch functionality

---
+++ src/launch/requestLaunch.ts
@@ -0,0 +1,29 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+type LaunchRequest = {
+    name: string,
+    destination: string,
+}
+
+type LaunchResponse = {
+    id: string,
+    status: "LAUNCHING" | "LAUNCHED"
+}
+
+export const handler = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const requestBody = event?.body;
+
+    if (!requestBody) {
+        throw Error("Bad Request")
+    }
+
+    const launchRequest: LaunchRequest = JSON.parse(requestBody);
+    // Make call to mission control to check mission status and launch if necessary
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({})
+    }
+}
+
+// POST /launch {  name: "", destination: ""  }
