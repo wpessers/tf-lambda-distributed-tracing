ccaef49258e73b0955c40ba5eed7d81af81a6864
warre <warre.pessers@gmail.com>
2025-01-26 16:29:43 +0100

    Trigger lambda replacement when code changes

--- infrastructure/lambda-launch.tf
+++ infrastructure/lambda-launch.tf
@@ -41,6 +41,7 @@ resource "aws_lambda_function" "request_launch" {
 
   filename = "../dist/lambdas.zip"
   handler  = "lambdas/requestLaunchLambda.js"
+  source_code_hash = filebase64sha256("../dist/lambdas.zip")
 
   runtime       = "nodejs18.x"
   architectures = ["arm64"]
--- infrastructure/lambda-mission.tf
+++ infrastructure/lambda-mission.tf
@@ -41,6 +41,7 @@ resource "aws_lambda_function" "control_mission" {
 
   filename = "../dist/lambdas.zip"
   handler  = "lambdas/controlMissionLambda.js"
+  source_code_hash = filebase64sha256("../dist/lambdas.zip")
 
   runtime       = "nodejs18.x"
   architectures = ["arm64"]
