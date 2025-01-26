ec28ee854238833a4b03b6e57b61db8464ed991a
warre <warre.pessers@gmail.com>
2025-01-26 15:37:23 +0100

    (WIP) Configure lambda infra

--- infrastructure/lambda-launch.tf
+++ infrastructure/lambda-launch.tf
@@ -1,7 +1,12 @@
 resource "aws_lambda_function" "launch" {
-  function_name = ""
+  function_name = "request-launch"
   role = ""
+
+  filename = "../dist/lambdas.zip"
+  handler = "lambdas/requestLaunchLambda.js"
+
   runtime = "nodejs18.x"
+  architectures = [ "arm64" ]
 
   tracing_config {
     mode = "PassThrough"
