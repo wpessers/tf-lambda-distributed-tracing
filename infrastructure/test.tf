a7b1c3aa558534f72d605c9aab3e762e73fc29c1
warre <warre.pessers@gmail.com>
2025-02-18 21:36:06 +0100

    Add test resource without additional env vars

--- infrastructure/test.tf
+++ infrastructure/test.tf
@@ -11,3 +11,12 @@ module "test" {
     TEST123 : "test123"
   }
 }
+
+module "test2" {
+  source = "./modules/otel-lambda-apigw"
+
+  name              = "test2"
+  filename          = "../dist/lambdas.zip"
+  handler           = "lambdas/requestLaunchLambda.handler"
+  api_execution_arn = aws_api_gateway_rest_api.launch.execution_arn
+}
