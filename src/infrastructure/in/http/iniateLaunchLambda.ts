ae5d526e3c77a3cc4bd1ca95b281d68b529f4e2f
warre <warre.pessers@gmail.com>
2025-02-28 22:52:22 +0100

    WIP refactorting

--- src/infrastructure/in/http/iniateLaunchLambda.ts
+++ src/infrastructure/in/http/iniateLaunchLambda.ts
@@ -1,30 +1,34 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-import LaunchRequest = Components.Schemas.LaunchRequest;
-import LaunchResponse = Components.Schemas.LaunchResponse;
+import InitiateLaunchRequest = Components.Schemas.InitiateLaunchRequest;
+import InitiateLaunchResponse = Components.Schemas.InitiateLaunchResponse;
 
 
-function mapToDomain() {
+function getRequestBody(event: APIGatewayProxyEvent): InitiateLaunchRequest {
     const requestBody = event.body;
 
     if (!requestBody) {
-        throw Error('Missing launch request')
+        throw Error('Missing launch request body')
     }
 
-    JSON.parse(requestBody) as LaunchRequest;
+    return JSON.parse(requestBody)
+}
+
+function mapToDomain(request: InitiateLaunchRequest):  {
+    throw new Error('Function not implemented.');
 }
 
 const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    
-    const { rocketName, destination } = JSON.parse(requestBody) as LaunchRequest;
 
-        return {
+    mapToDomain(getRequestBody(event))
+
+    return {
         statusCode: 200,
         body: JSON.stringify({
             rocketName,
             destination,
             status
-        } as LaunchResponse)
+        } as InitiateLaunchResponse)
     }
 }
 
